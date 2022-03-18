5aefd11ef5f95146f24e67bd6fc1c9815e6c04b9
LibXZR <i@xzr.moe>
2022-03-18 10:12:37 +0800

    base: Add support for window ignore secure
    Feature: yt/KSCOPE-23
    Change-Id: Ia9447ca17f6b24152b64526c25710a77e1388ce6
    Signed-off-by: LibXZR <i@xzr.moe>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10244,6 +10244,13 @@ public final class Settings {
         public static final String GOOGLE_PHOTOS_SPOOF = "google_photos_spoof";
 
         /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
